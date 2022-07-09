609d08c3f36a0316f0b05f2ed1033803c9c293ae
esgar <navaedgar36@yahoo.com.mx>
2022-07-09 17:51:42 -0500

    seeds created

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { setContext } from '@apollo/client/link/context';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// import Home from './pages/Home';
+ import Home from './pages/Home';
 // import Login from './pages/Login';
 // import NoMatch from './pages/NoMatch';
 // import SingleThought from './pages/SingleThought';
@@ -45,11 +45,11 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              {/* <Route 
+               <Route 
                 path="/" 
                 element={<Home />} 
               />
-              <Route 
+              {/*<Route 
                 path="/login" 
                 element={<Login />} 
               />
